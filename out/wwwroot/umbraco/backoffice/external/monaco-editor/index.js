54e02b684a715ecd127a6465b9dfe5d54bfa8e97
Stefan Strandberg <stefan.m.strandberg@hotmail.com>
2025-11-09 22:39:24 +0100

    azure

--- out/wwwroot/umbraco/backoffice/external/monaco-editor/index.js
+++
@@ -1,6 +0,0 @@
-import { a as s, e } from "./index-D9CSIjI1.js";
-export {
-  s as monaco,
-  e as styles
-};
-//# sourceMappingURL=index.js.map
--- out/wwwroot/umbraco/backoffice/libs/context-api/consume/index.js
+++
@@ -1,3 +0,0 @@
-export * from './context-consumer.controller.js';
-export * from './context-consumer.js';
-export * from './context-request.event.js';
